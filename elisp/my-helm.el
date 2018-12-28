29fbe2e3d4215aa298eef96859c0f1cdb9fe87b4
joseph.yoon <joseph.yoon@kakaocorp.com>
2018-12-28 23:29:29 +0900

    helm 설정 추가

---
+++ elisp/my-helm.el
@@ -0,0 +1,19 @@
+(use-package helm
+  :ensure t
+  )
+
+(use-package projectile
+  :ensure t
+  :bind (("C-c s" . projectile-switch-open-project) ("C-c p" . projectile-switch-project))
+  :config
+    (projectile-global-mode)
+    (setq projectile-enable-caching t)
+    )
+
+(use-package helm-projectile
+  :ensure t
+  :bind ("S-p" . helm-projectile-find-file)
+  :config
+    (helm-projectile-on))
+
+(provide 'my-helm)
--- init.el
+++ init.el
@@ -17,12 +17,16 @@
 
 (require 'my-git)
 
+(require 'my-helm)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (use-package monokai-theme magit))))
+ '(package-selected-packages
+   (quote
+    (helm-projectile projectile helm use-package monokai-theme magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
