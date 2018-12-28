993486bfcd64c714174bad81193345177eaff080
joseph.yoon <joseph.yoon@kakaocorp.com>
2018-12-28 23:44:37 +0900

    창 최대 크기로 수정

--- elisp/my-display.el
+++ elisp/my-display.el
@@ -8,16 +8,22 @@
   :config
   (load-theme 'monokai t))
 
-(when I-am-on-MacOSX
-  (setq default-frame-alist
-     '((width . 195)
-       (height . 50)))
-  )
-(when I-am-on-Linux
-  (setq default-frame-alist
-     '((width . 130)
-       (height . 45)))
-  )
+;; (when I-am-on-MacOSX
+;;   (setq default-frame-alist
+;;      '((width . 195)
+;;        (height . 50)))
+;;   )
+;; (when I-am-on-Linux
+;;   (setq default-frame-alist
+;;      '((width . 130)
+;;        (height . 45)))
+;;   )
+
+;; 풀 스크린으로
+(add-hook 'window-setup-hook 'toggle-frame-fullscreen t)
+;; 창 최대 크기로
+;;(add-hook 'window-setup-hook 'toggle-frame-maximized t)
+
 
 (prefer-coding-system 'utf-8)
 ;;(set-default-coding-system 'utf-8)
