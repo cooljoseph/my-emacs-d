6c7712a5a9cdf13002248a8e5542a3cc911647aa
cooljoseph <sidnancy81@gmail.com>
2019-01-07 23:11:49 +0900

    Markdown Mode 실패 다시 도전

--- init.el
+++ init.el
@@ -24,6 +24,8 @@
 
 (use-package my-yasnippet)
 
+(use-package my-markdown)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
